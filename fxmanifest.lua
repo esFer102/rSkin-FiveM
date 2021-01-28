495f5fd716b5712cde4a8dd4f65474400dab9e4b
esFer <58149189+esFer102@users.noreply.github.com>
2021-01-28 15:53:11 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Do not delete this to use my script
+-- Script created by esFerDev. Uploaded from https://github.com/esFer102/rSkin-FiveM
+
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Comando de /rskin para recargar tu personaje | Command /rskin to reload your skin.'
+
+version '1.0'
+
+client_script 'client.lua'
